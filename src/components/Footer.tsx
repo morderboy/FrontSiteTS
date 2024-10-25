65ecc5903a9c8a2c59bf8ce8e895c0a569d5b5c3
morderboy <slava137267@gmail.com>
2024-10-25 18:31:41 +1000

    make carcases

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+// src/components/Footer.tsx
+export const Footer = () => {
+    return (
+        <footer>
+            {/*Ваш код*/}
+        </footer>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+// src/components/Header.tsx
+export const Header = () => {
+    return (
+        <header>
+        {/* Ваш код */}
+        </header>
+    );
+};
