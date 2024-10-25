f05599b09aa21e6957ef655589dba022b648116b
morderboy <slava137267@gmail.com>
2024-10-25 19:01:51 +1000

    add 3 new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
+import { AboutPage } from './pages/AboutPage'
+import { SkillsPage } from './pages/SkillsPage'
+import { ProjectsPage } from './pages/ProjectsPage'
+import { ContactPage } from './pages/ContactPage'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage />} /> 
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage/>} />
+        <Route path='/skills' element={<SkillsPage/>} />
+        <Route path='/projects' element={<ProjectsPage />} />
+        <Route path='/contact' element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
   )
